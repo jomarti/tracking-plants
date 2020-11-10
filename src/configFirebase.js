07e2d3749f1519562a04baf70050063a76b160cb
Jordi Marti <jormara2@gmail.com>
2020-11-10 22:16:54 +0100

    feat: add firebase config

---
+++ src/configFirebase.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: 'tracking-plants-2b9f5.firebaseapp.com',
+  databaseURL: 'https://tracking-plants-2b9f5.firebaseio.com',
+  projectId: 'tracking-plants-2b9f5',
+  storageBucket: 'tracking-plants-2b9f5.appspot.com',
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SEND_ID,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const database = firebase.firestore();
+
+const myPlantsCollection = database.collection('my plants');
+
+export { database, myPlantsCollection };
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import '@/assets/styles';
 import Vue from 'vue';
 import App from './App.vue';
+import './configFirebase';
 import './registerServiceWorker';
 import router from './router';
 import store from './store';
