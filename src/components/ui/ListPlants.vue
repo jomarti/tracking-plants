fec3440f6fee43bcf52e07a47900f68a2087c1ca
Jordi Marti <jormara2@gmail.com>
2020-11-10 09:36:27 +0100

    feat: add tailwind & create listplants component

---
+++ src/components/ui/ListPlants.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <h1 class="title font-bold text-xl mb-4">
+      Mis plantas
+    </h1>
+    <ul>
+      <li>
+        <button class="border border-black text-3xl rounded-full py-1 px-3">
+          +
+        </button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ListPlants'
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import '@/assets/styles';
 import Vue from 'vue';
 import App from './App.vue';
 import './registerServiceWorker';
